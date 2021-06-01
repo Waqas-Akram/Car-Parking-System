4dcdff6af7fd4d8cd3e6f9e65a99810fab658f36
Waqas Akram <waqasassasin@gmail.com>
2021-06-01 10:47:40 +0500

    Added files in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import React,{Fragment} from 'react'
-
+import React, { Fragment, useState, useEffect } from "react";
+import NavHeader from "./components/layout/NavHeader";
+import Routing from "./routes/Routing";
+import { BrowserRouter as Router } from "react-router-dom";
+import Loader from './components/animations/loader/Loader'
+import Footer from './components/footer/Footer'
+import ScrollToTop from './components/scroll/ScrollToTop'
+import ScrollToTopButton from './components/scroll/ScrollToTopButton'
 const App = () => {
-  return (
-    <Fragment>
-      <h1>Hello</h1>
-    </Fragment>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    window.load = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
 
-export default App
+  return loading ? (
+    <Loader />
+  ) : (
+    <Router>
+      <Fragment>
+        <NavHeader />
+        <ScrollToTop/>
+        <Routing />
+        <ScrollToTopButton/>
+        <Footer/>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
