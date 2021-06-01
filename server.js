5c7ccd41b19831e13688ca6b541631b7956385cf
Waqas Akram <waqasassasin@gmail.com>
2021-06-01 11:28:37 +0500

    ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const contact = require("./backend/routes/api/contact");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
