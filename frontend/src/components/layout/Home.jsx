822635beb0be9e9d621bdf0c626f88ab0d19884e
Waqas Akram <waqasassasin@gmail.com>
2021-06-11 01:40:30 +0500

    Getting and displaying slots

--- frontend/src/components/layout/Home.jsx
+++ frontend/src/components/layout/Home.jsx
@@ -1,47 +1,162 @@
-import React, { Fragment } from "react";
-import { Jumbotron, Row, Col, Container } from "react-bootstrap";
-import DashboardCard from "./DashboardCard";
+import React, { Fragment, useState, useEffect } from "react";
+import { Jumbotron, Row, Col, Container, Table } from "react-bootstrap";
 import ParkingAnimation from "../animations/parking/ParkingAnimation";
+import axios from "axios";
 
 const Home = () => {
+  const [slots, setSlots] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => getSlotsData(), 100);
+  }, []);
+
+  const getSlotsData = async () => {
+    const res = await axios.get("/api/sqlconnection/slots");
+    console.log(res.data);
+    setSlots(res.data);
+  };
   return (
     <Fragment>
       <Container>
-        <Jumbotron className="p-2 bg-white my-3">
+        <Jumbotron className="p-2 bg-white mt-3">
           <h1 className="text-center mt-1" style={{ color: "gray" }}>
-            DashBoard
+            Slots
           </h1>
         </Jumbotron>
-        <Jumbotron className="my-3 bg-white ">
-          <Row>
-            <Col>
-              <DashboardCard
-                background="bg-info"
-                value="20"
-                title="Total Space"
-              />
-            </Col>
-            <Col>
-              <DashboardCard
-                background="bg-success"
-                value="10"
-                title="Available Space"
-              />
-            </Col>
-            <Col>
-              <DashboardCard
-                background="bg-warning"
-                value="10"
-                title="Car Parked"
-              />
-            </Col>
-          </Row>
-        </Jumbotron>
+        <center>
+          <Table size="sm" responsive>
+            <thead>
+              <tr>
+                <th>Parking Slot</th>
+                <th>Occupancy indicator</th>
+                <th>Occupancy Message</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>P1</td>
+                <td>
+                  {slots.Slot1 === "0" ? (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "red" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(168, 247, 191)" }}
+                      ></div>
+                    </>
+                  ) : (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(247, 175, 175)" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "green" }}
+                      ></div>
+                    </>
+                  )}
+                </td>
+                <td>{slots.Slot1 === "0" ? <>occupied</> : <>vacant</>}</td>
+              </tr>
+              <tr>
+                <td>P2</td>
+                <td>
+                  {slots.Slot2 === "0" ? (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "red" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(168, 247, 191)" }}
+                      ></div>
+                    </>
+                  ) : (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(247, 175, 175)" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "green" }}
+                      ></div>
+                    </>
+                  )}
+                </td>
+                <td>{slots.Slot2 === "0" ? <>occupied</> : <>vacant</>}</td>
+              </tr>
+              <tr>
+                <td>P3</td>
+                <td>
+                  {slots.Slot3 === "0" ? (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "red" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(168, 247, 191)" }}
+                      ></div>
+                    </>
+                  ) : (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(247, 175, 175)" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "green" }}
+                      ></div>
+                    </>
+                  )}
+                </td>
+                <td>{slots.Slot3 === "0" ? <>occupied</> : <>vacant</>}</td>
+              </tr>
+              <tr>
+                <td>P4</td>
+                <td>
+                  {slots.Slot4 === "0" ? (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "red" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(168, 247, 191)" }}
+                      ></div>
+                    </>
+                  ) : (
+                    <>
+                      <div
+                        className="circle"
+                        style={{ background: "rgb(247, 175, 175)" }}
+                      ></div>
+                      <div
+                        className="circle"
+                        style={{ background: "green" }}
+                      ></div>
+                    </>
+                  )}
+                </td>
+                <td>{slots.Slot4 === "0" ? <>occupied</> : <>vacant</>}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </center>
         <Jumbotron className="bg-white">
           <Row>
             <Col>
               <h3 style={{ color: "gray" }}>
-                <q>Park Where You are , Not Where You Are'nt</q>
+                <q>Park Where You are , Not Where You Are'nt </q>
               </h3>
             </Col>
             <Col>
