656bdb522b4e8e8cf83748637393b7f1d8f45bf3
Waqas Akram <waqasassasin@gmail.com>
2021-06-13 12:13:17 +0500

    Sql crud operations enabled

--- backend/routes/api/sqlconnection.js
+++ backend/routes/api/sqlconnection.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 const pool = createPool({
   host: "sql6.freesqldatabase.com",
-  user: "sql6417218",
-  password: "cMzE8wtLnP",
-  database: "sql6417218",
+  user: "sql6418689",
+  password: "keADUqGCwu",
+  database: "sql6418689",
   connectionLimit: 10,
 });
 
@@ -34,11 +34,37 @@ router.get("/timestamps", async (req, res) => {
 router.delete("/delete/:id", async (req, res) => {
   await pool.query(
     "DELETE from Registrations where id = ?",
-    [req.params.id],
+    req.params.id,
     (err, rows) => {
-      pool.release();
       if (err) throw err;
       res.status(200).json({ msg: "Successfull deleted" });
+    }
+  );
+});
+
+//update user by using it's id
+
+router.put("/registrations/update/:id", async (req, res) => {
+  const { Name, RFID } = req.body;
+  await pool.query(
+    "UPDATE Registrations SET Name = ? , RFID = ? WHERE id = ?",
+    [Name, RFID, req.params.id],
+    (err, rows) => {
+      if (err) throw err;
+      res.status(200).json({ msg: "Successfull updated" });
+    }
+  );
+});
+
+//get single user by id
+
+router.get("/registrations/:id", async (req, res) => {
+  await pool.query(
+    "SELECT * from Registrations where id = ?",
+    req.params.id,
+    (err, rows) => {
+      if (err) throw err;
+      res.json(rows[0]);
     }
   );
 });
@@ -50,6 +76,20 @@ router.get("/slots", async (req, res) => {
     if (err) throw err;
     res.json(rows[0]);
   });
+});
+
+//post data in registration table with
+
+router.post("/register", async (req, res) => {
+  const { Name, RFID } = req.body;
+  await pool.query(
+    `INSERT INTO Registrations (Name , RFID , Status) VALUES (? , ? , ?)`,
+    [Name, RFID, "0"],
+    (err, rows) => {
+      if (err) throw err;
+      res.json(rows);
+    }
+  );
 });
 
 module.exports = router;
