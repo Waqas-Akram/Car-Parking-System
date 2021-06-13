bbefe1953b23c42ef901f3d1e8226d5ca66d9ebf
Waqas Akram <waqasassasin@gmail.com>
2021-06-13 12:42:49 +0500

    delete user and update user

--- frontend/src/components/admin/RegistrationTable.jsx
+++ frontend/src/components/admin/RegistrationTable.jsx
@@ -1,26 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Moment from "react-moment";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import { Button } from "react-bootstrap";
-// import axios from 'axios'
-// import Swal from 'sweetalert2'
+import axios from "axios";
+import Swal from "sweetalert2";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import UpdateUser from "../form/UpdateUser";
 
 const RegistrationTable = ({ id, rfid, Name, Status, reg_date }) => {
-  // const deleteUser = async(user_id)=>{
-  //   const res = await axios.delete(`/api/sqlconnection/delete/${user_id}`);
-  //   console.log(res.data);
-  //   if(res.status === 200){
-  //     Swal.fire({
-  //       text:"User deleted Successfully",
-  //       icon:"success"
-  //     })
+  const [open, setOpen] = useState(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
 
-  //   }
-
-  // }
+  const deleteUser = async (user_id) => {
+    const res = await axios.delete(`/api/sqlconnection/delete/${user_id}`);
+    console.log(res.data);
+    if (res.status === 200) {
+      Swal.fire({
+        text: res.data.msg,
+        icon: "success",
+      });
+    }
+  };
   return (
     <Fragment>
+      <Modal open={open} onClose={onCloseModal} center>
+        <UpdateUser id={id} />
+      </Modal>
+
       <tr>
         <td>{id}</td>
         <td>{rfid}</td>
@@ -37,15 +46,12 @@ const RegistrationTable = ({ id, rfid, Name, Status, reg_date }) => {
           <Moment format="dddd, MMMM Do YYYY, h:mm:ss a">{reg_date}</Moment>
         </td>
         <td>
-          <Button
-            onClick={() => alert("Hello Edit")}
-            className="border-0 bg-white  p-1"
-          >
+          <Button onClick={onOpenModal} className="border-0 bg-white  p-1">
             {" "}
             <EditIcon className="text-info" />
           </Button>
           <Button
-            onClick={() => alert("delete button")}
+            onClick={() => deleteUser(id)}
             className="border-0 bg-white  p-1"
           >
             {" "}
